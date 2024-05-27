8acb9aa64a97ef09e8b2e92011eb1b606318735e
Ziad-Helal <zgalhelaly@gmail.com>
2024-05-27 15:59:15 +0300

    e

--- controllers/leads-scraper.js
+++ controllers/leads-scraper.js
@@ -41,7 +41,7 @@ async function sendMultiRequests(endPoint, response, params) {
     {
       ...params,
       cursor:
-        "eyJnbWFwX3BsYWNlX2lkIjoxNjAxODc3OSwiX3BvaW50c1RvTmV4dEl0ZW1zIjp0cnVlfQ",
+        "eyJnbWFwX3BsYWNlX2lkIjoxNjQyODg0NSwiX3BvaW50c1RvTmV4dEl0ZW1zIjp0cnVlfQ",
     },
     c
   );
@@ -78,8 +78,8 @@ async function sendScrappingRequest(url, response, params, counter) {
     c++;
   } while (result.meta.status != "completed");
   console.log(
-    `${counter * result.meta.per_page + 5000} / ${result.meta.count}\n${
-      result.meta.count - counter * result.meta.per_page - 5000
+    `${counter * result.meta.per_page + 7500} / ${result.meta.count}\n${
+      result.meta.count - counter * result.meta.per_page - 7500
     } remaining...`
   );
 
